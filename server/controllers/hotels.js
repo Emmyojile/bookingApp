4f991fb95354dc3d9c2881ffb870e8ab4ad0563e
Emmyojile <emmaojile99@gmail.com>
2023-10-13 06:56:43 -0800

    adding admin panel

--- server/controllers/hotels.js
+++ server/controllers/hotels.js
@@ -3,160 +3,169 @@ import Room from "../models/Room.js";
 
 //Create Hotel
 export const createHotel = async (req, res, next) => {
-    const newHotel = new Hotel(req.body);
-    try {
-        const savedHotel = await newHotel.save();
-        return res.status(200).json(savedHotel)
-    } catch (err) {
-        next(err);
-    }
-}
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    return res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Update Hotel
-export const updateHotel = async (req, res, next) => {    
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
-        return res.status(200).json(updatedHotel)
-    } catch (err) {
-        next(err);
-    }
-}
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    return res.status(200).json(updatedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Delete Hotel
-export const deleteHotel = async (req, res, next) => {    
-    try {
-        await Hotel.findByIdAndDelete(req.params.id);
-        return res.status(200).json("Hotel deleted Successfully");
-    } catch (err) {
-        next(err);
-    }
-}
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    return res.status(200).json("Hotel deleted Successfully");
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Get Single Hotel
-export const singleHotel = async (req, res, next) => {    
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        return res.status(200).json(hotel)
-    } catch (err) {
-        next(err);
-    }
-}
+export const singleHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    return res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Get All Hotels
-export const getAllHotels = async (req, res, next) => {    
-    try {
-        const { featured, name, sort, fields, filterOptions, min, max } = req.query;
-        const queryObject = {};
+export const getAllHotels = async (req, res, next) => {
+  try {
+    const { featured, name, sort, fields, filterOptions, min, max } = req.query;
+    const queryObject = {};
 
-        if (featured){
-            queryObject.featured = featured === 'false' ? false : true;
-        }
-
-        if (name){
-            queryObject.name = { $regex: name, $options: 'i' };
-        }
-        
-        if (filterOptions){
-            const operatorMap = {
-                '>': '$gt',
-                '>=': '$gte',
-                '=' : '$eq',
-                '<': '$lt',
-                '<=': '$lte',
-            };
-            
-            const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-            let filters = filterOptions.replace(
-                regEx,
-                (match) => `-${operatorMap[match]}-`
-            );
-            
-            const options = ["cheapestPrice", "rating"];
-            
-            filters = filters.split(',').forEach((item) => {
-                const [field, operator, value] = item.split('-');
-                if (options.includes(field)){
-                    queryObject[field] = { [operator]: Number(value) };
-                }
-            });
-        }
-        
-        if (min) {
-            queryObject.cheapestPrice = { $gte: Number(min) };
-        }
-
-        if (max) {
-            queryObject.cheapestPrice = { ...queryObject.cheapestPrice, $lte: Number(max) };
-        }
-        
-        let result = Hotel.find(queryObject);
-        
-        if (sort){
-            const sortList = sort.split(',').join(' ')
-            result = result.sort(sortList);
-        } else {
-            result = result.sort('createdAt');
-        }
-        
-        if (fields){
-            const fieldsList = fields.split(',').join(' ')
-            result = result.select(fieldsList);
-        }
-        
-        const limit = Number(req.query.limit) || 9;
-        
-        const allHotels = await result.limit(limit);
-        return res.status(200).json(allHotels);
-    } catch (err) {
-        console.error(err);
-        next(err);
+    if (featured) {
+      queryObject.featured = featured === "false" ? false : true;
     }
-}
 
+    if (name) {
+      queryObject.name = { $regex: name, $options: "i" };
+    }
 
+    if (filterOptions) {
+      const operatorMap = {
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq",
+        "<": "$lt",
+        "<=": "$lte",
+      };
+
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = filterOptions.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+      );
+
+      const options = ["cheapestPrice", "rating"];
+
+      filters = filters.split(",").forEach((item) => {
+        const [field, operator, value] = item.split("-");
+        if (options.includes(field)) {
+          queryObject[field] = { [operator]: Number(value) };
+        }
+      });
+    }
+
+    if (min) {
+      queryObject.cheapestPrice = { $gte: Number(min) };
+    }
+
+    if (max) {
+      queryObject.cheapestPrice = {
+        ...queryObject.cheapestPrice,
+        $lte: Number(max),
+      };
+    }
+
+    let result = Hotel.find(queryObject);
+
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("createdAt");
+    }
+
+    if (fields) {
+      const fieldsList = fields.split(",").join(" ");
+      result = result.select(fieldsList);
+    }
+
+    const limit = Number(req.query.limit) || 9;
+
+    const allHotels = await result.limit(limit);
+    return res.status(200).json(allHotels);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
 
 // Get Hotels by Count
-export const countByCity = async (req, res, next) => {    
-        const cities = req.query.cities.split(',');
-    try {
-        const list = await Promise.all(cities.map(city => {
-            return Hotel.countDocuments({city: city})
-        }))
-        return res.status(200).json(list)
-    } catch (err) {
-        next(err);
-    }
-}  
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    return res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Get Properties by type
-export const countByType = async (req, res, next) => {    
-    try {
-        const hotelCount = await Hotel.countDocuments({type: "hotel"});
-        const apartmentCount = await Hotel.countDocuments({type: "apartment"});
-        const resortCount = await Hotel.countDocuments({type: "resort"});
-        const villaCount = await Hotel.countDocuments({type: "villa"});
-        const cabinCount = await Hotel.countDocuments({type: "cabin"});
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
 
-        res.status(200).json([
-            {type: "hotel", count: hotelCount},
-            {type: "apartments", count: apartmentCount},
-            {type: "resorts", count: resortCount},
-            {type: "villas", count: villaCount},
-            {type: "cabins", count: cabinCount},
-        ])
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getHotelRooms = async (req, res, next) => {    
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        const list = await Promise.all(hotel.rooms.map((room) => {
-            return Room.findById(room)
-        }));
-        return res.status(200).json(list);
-    } catch (err) {
-        next(err);
-    }
-}
+export const getHotelRooms = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    const list = await Promise.all(
+      hotel.rooms.map((room) => {
+        return Room.findById(room);
+      })
+    );
+    return res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -1,84 +1,92 @@
 import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
-import {createError} from "../utils/error.js";
+import { createError } from "../utils/error.js";
 
 export const createRoom = async (req, res, next) => {
-    const hotelId = req.params.hotelId;
-    const newRoom = new Room(req.body);
+  const hotelId = req.params.hotelId;
+  const newRoom = new Room(req.body);
 
+  try {
+    const savedRoom = await newRoom.save();
     try {
-        const savedRoom = await newRoom.save();
-        try {
-            await Hotel.findByIdAndUpdate(hotelId, {$push: {rooms: savedRoom._id}})
-        } catch (err) {
-            next(err);
-        }      
-        res.status(200).json(savedRoom);
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
 };
 
 //Update Room
-export const updateRoom = async (req, res, next) => {    
-    try {
-        const updatedRoom = await Room.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
-        return res.status(200).json(updatedRoom)
-    } catch (err) {
-        next(err);
-    }
-}
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    return res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const updateRoomAvailability = async (req, res, next) => {    
-    try {
-        await Room.updateOne(
-            {"roomNumbers._id": req.params.id},
-            {
-                $push: {
-                    "roomNumbers.$.unavailableDates": req.body.dates 
-                },
-            }
-        )
-        return res.status(200).json("Room Status has been updated")
-    } catch (err) {
-        next(err);
-    }
-}
+export const updateRoomAvailability = async (req, res, next) => {
+  try {
+    await Room.updateOne(
+      { "roomNumbers._id": req.params.id },
+      {
+        $push: {
+          "roomNumbers.$.unavailableDates": req.body.dates,
+        },
+      }
+    );
+    return res.status(200).json("Room Status has been updated");
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Delete Room
-export const deleteRoom = async (req, res, next) => {    
-    const hotelId = req.params.hotelId;
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelId;
+
+  try {
+    await Room.findByIdAndDelete(req.params.id);
 
     try {
-        await Room.findByIdAndDelete(req.params.id);
-
-        try {
-            await Hotel.findByIdAndUpdate(hotelId, {$pull: {rooms: req.params.id}});
-        } catch (err) {
-            next(err);
-        } 
-        return res.status(200).json("Room deleted Successfully");
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
     } catch (err) {
-        next(err);
+      next(err);
     }
-}
+    return res.status(200).json("Room deleted Successfully");
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Get Single Room
-export const singleRoom = async (req, res, next) => {    
-    try {
-        const room = await Room.findById(req.params.id);
-        return res.status(200).json(room)
-    } catch (err) {
-        next(err);
-    }
-}
+export const singleRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    return res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
 
 //Get All Room
-export const getAllRooms = async (req, res, next) => {    
-    try {
-        const allRooms = await Room.find();
-        return res.status(200).json(allRooms)
-    } catch (err) {
-        next(err);
-    }
-}
+export const getAllRooms = async (req, res, next) => {
+  try {
+    const allRooms = await Room.find();
+    return res.status(200).json(allRooms);
+  } catch (err) {
+    next(err);
+  }
+};
