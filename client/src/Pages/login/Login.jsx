2b16451a1158661cb9343c282e58c0b806e00edd
Emmyojile <emmaojile99@gmail.com>
2023-10-13 17:41:22 -0800

    working on booking branch

--- client/src/Pages/login/Login.jsx
+++ client/src/Pages/login/Login.jsx
@@ -1,47 +1,60 @@
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
 
-import React, { useContext, useState} from 'react'
+import React, { useContext, useState } from "react";
 
 const Login = () => {
-    const [credentials, setCredentials] = useState({
-        username: undefined,
-        password: undefined,
-    });
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
 
-    const {loading, error, dispatch} = useContext(AuthContext);
+  const { loading, error, dispatch } = useContext(AuthContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        setCredentials((prev) => ({...prev, [e.target.id]: e.target.value}));
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post(`/login`, credentials);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      navigate("/");
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
-
-    const handleClick = async (e) => {
-        e.preventDefault();
-        dispatch({type: "LOGIN_START"})
-        try {
-            const res = await axios.post(`/login`, credentials);
-            dispatch({type: "LOGIN_SUCCESS", payload: res.data});
-            navigate("/");
-        } catch (err) {
-            dispatch({type: "LOGIN_FAILURE", payload: err.response.data})
-        }
-    }
-
+  };
 
   return (
     <div className="login">
-        <div className="lContainer">
-            <input type="text" placeholder="username" id="username" onChange={handleChange} className="lInput" />
-            <input type="password" placeholder="password" id="password" onChange={handleChange} className="lInput" />
-            <button disabled={loading} onClick={handleClick} className="lButton">Login</button>
-            {error && <span>{error.message}</span>}
-        </div>
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <button disabled={loading} onClick={handleClick} className="lButton">
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,43 +1,46 @@
 import User from "../models/User.js";
-import {createError} from "../utils/error.js";
+import { createError } from "../utils/error.js";
 
+export const register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    // if(!username || !password || !email) {
+    //     res.json({error: "Please provide all required fields"})
+    // }
 
-export const register = async (req, res, next) => {    
-    try {
-        const {username, email, password} = req.body;
-        // if(!username || !password || !email) {
-        //     res.json({error: "Please provide all required fields"})
-        // }
-        
-        // const userNameExists = await User.findOne({username})
-        // if(userNameExists) {
-        //     res.json({error: 'This username is already taken'})
-        // }
+    // const userNameExists = await User.findOne({username})
+    // if(userNameExists) {
+    //     res.json({error: 'This username is already taken'})
+    // }
 
-        const newUser = await User.create({...req.body})
-        return res.status(201).json("User created successfully");
-    } catch (err) {
-        next(err);
+    const newUser = await User.create({ ...req.body });
+    return res.status(201).json("User created successfully");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found ooo!"));
+
+    const suppliedPassword = await user.comparePassword(
+      req.body.password,
+      user.password
+    );
+    if (!suppliedPassword) {
+      return next(createError(400, "Password incorrect!"));
     }
-}
 
-export const login = async (req, res, next) => {    
-    try {
-        const user = await User.findOne({username: req.body.username});
-        if(!user) 
-            return next(createError(404, "User not found ooo!"));
-        
+    const token = user.createJWT();
+    const { password, isAdmin, ...otherDetails } = user._doc;
 
-        const suppliedPassword = await user.comparePassword(req.body.password, user.password);
-        if(!suppliedPassword) {
-            return next(createError(400, "Password incorrect!"))
-        }
-
-        const token = user.createJWT()
-        const {password, isAdmin, ...otherDetails} = user._doc;
-
-        return res.cookie("access_token", token,{httpOnly: true,}).status(200).json({...otherDetails});
-    } catch (err) {
-        next(err);
-    }
-}
+    return res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ details: { ...otherDetails }, isAdmin });
+  } catch (err) {
+    next(err);
+  }
+};
